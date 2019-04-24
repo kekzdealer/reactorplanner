fc039b9eea0cfc0fa03311f53bfd6fe375243e63
Christina Berchtold <kekzdealer@gmail.com>
2019-04-25 00:47:18 +0200

    hooked up reactor simulation

--- src/graphics/GraphicalUserInterface.java
+++ src/graphics/GraphicalUserInterface.java
@@ -2,6 +2,8 @@ package graphics;
 
 import java.awt.Color;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
@@ -10,13 +12,17 @@ import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EtchedBorder;
 
 import logic.ComponentFactory;
 import logic.ComponentFactory.ComponentType;
+import logic.Reactor;
+import logic.StatusReport;
 
 @SuppressWarnings("serial")
 public class GraphicalUserInterface extends JFrame {
@@ -30,6 +36,8 @@ public class GraphicalUserInterface extends JFrame {
 	public static ComponentType selectedComponent;
 	
 	public static void main(String[] args) {
+		
+		final Reactor reactor = new Reactor();
 		
 		// Load resources
 		try {
@@ -77,27 +85,58 @@ public class GraphicalUserInterface extends JFrame {
 		reactorPanel.addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent me) {
 				final Point mousePos = reactorPanel.getMousePosition();
+				final int slotX = (int) Math.floor(mousePos.getX() / 67);
+				final int slotY = (int) Math.floor(mousePos.getY() / 67);
 				if(me.getButton() == MouseEvent.BUTTON1) {
-					reactorPanel.addComponent(iconData.get(partPicker.getSelectedItem()), 
-							(int) Math.floor(mousePos.getX() / 67), 
-							(int) Math.floor(mousePos.getY() / 67));					
+					reactorPanel.addComponent(iconData.get(partPicker.getSelectedItem()), slotX, slotY);
+					reactor.insertComponent((ComponentType) partPicker.getSelectedItem(), slotX, slotY);
 				} else if (me.getButton() == MouseEvent.BUTTON3) {
-					reactorPanel.addComponent(emptyComponent, 
-							(int) Math.floor(mousePos.getX() / 67), 
-							(int) Math.floor(mousePos.getY() / 67));
+					reactorPanel.addComponent(emptyComponent, slotX, slotY);
+					// TODO : remove component from reactor when removed from UI
 				}
 				reactorPanel.repaint();
 			}
 		});
 		gui.add(reactorPanel);
 		
-		
 		JPanel infoPanel = new JPanel();
 		infoPanel.setLocation(10, 450);
 		infoPanel.setSize(972, 207);
 		infoPanel.setBackground(Color.LIGHT_GRAY);
 		infoPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+		infoPanel.setLayout(null);
 		gui.add(infoPanel);
+		JLabel labelHullHeat = new JLabel("Hull Heat: ");
+		JLabel labelEUOutput = new JLabel("EU/t: ");
+		JLabel labelHUOutput = new JLabel("Hu/s ");
+		labelHullHeat.setSize(200, 30);
+		labelEUOutput.setSize(200, 30);
+		labelHUOutput.setSize(200, 30);
+		labelHullHeat.setLocation(3, 3);
+		labelEUOutput.setLocation(3, 50);
+		labelHUOutput.setLocation(3, 100);
+		infoPanel.add(labelHullHeat);
+		infoPanel.add(labelEUOutput);
+		infoPanel.add(labelHUOutput);
+		
+		JButton toggleReactorButton = new JButton("START");
+		toggleReactorButton.setSize(128, 32);
+		toggleReactorButton.setLocation(650, 200);
+		toggleReactorButton.setBackground(Color.GREEN);
+		toggleReactorButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				toggleReactorButton.setText(reactor.isRunning() ? "STOP" : "START");
+				toggleReactorButton.setBackground(reactor.isRunning() ? Color.RED : Color.GREEN);
+				if(reactor.isRunning()) {
+					reactor.setOff();
+				}
+				else {
+					reactor.setOn();
+					reactor.hookReactor(new StatusReport(labelHullHeat, labelEUOutput, labelHUOutput));
+				}
+			}
+		});
+		gui.add(toggleReactorButton);
 		
 		gui.setVisible(true);
 				
