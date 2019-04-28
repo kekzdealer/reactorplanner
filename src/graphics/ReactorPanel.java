b4b4903a39de4b0917f90cf4e09d05ae6df4ecde
kekzdealer <kekzdealer@gmail.com>
2019-04-29 00:39:43 +0200

    fixed durability bar colours

--- src/graphics/ReactorPanel.java
+++ src/graphics/ReactorPanel.java
@@ -27,6 +27,7 @@ public class ReactorPanel extends JPanel {
 
 	private final BufferedImage[][] components = new BufferedImage[9][6];	
 	private final boolean[][] componentValidation = new boolean[9][6];
+	private final int[][] durabilityBarLengths = new int[9][6];
 
 	
 	public ReactorPanel() {
@@ -78,10 +79,27 @@ public class ReactorPanel extends JPanel {
 		}
 		// Draw reactor overlay
 		g.drawImage(reactorOverlay, 1, 1, this);
+		for(int x = 0; x < components.length; x++) {
+			for(int y = 0; y < components[0].length; y++) {
+				// -> reset bars to gray
+				g.setColor(new Color(198, 198, 198, 255));
+				g.fillRect((4 + x * 67), (66 + y * 67), 64, 3);
+				// -> draw new bars
+				if(durabilityBarLengths[x][y] >= 32) {
+					g.setColor(new Color(0, 255, 0, 255));
+				} else if((durabilityBarLengths[x][y] >= 16) && (durabilityBarLengths[x][y] < 32)) {
+					g.setColor(new Color(255, 127, 40, 255));
+				} else {
+					g.setColor(new Color(255, 0, 0, 255));
+				}
+				final int startX = 4 + x * 67;
+				final int startY = 66 + y * 67;
+				g.fillRect(startX, startY, durabilityBarLengths[x][y], 3);
+			}
+		}
 		g.dispose();
 	}
 	
-	// TODO: fix durability bar color
 	public void updateDurabilities(HashSet<ReactorComponent> rcs) {
 		// validate components (used in paintComponent() to check for outdated state)
 		for(int x = 0; x < componentValidation.length; x++) {
@@ -93,15 +111,7 @@ public class ReactorPanel extends JPanel {
 			componentValidation[rc.getX()][rc.getY()] = true;
 		}
 		
-		final Graphics og = reactorOverlay.getGraphics();
-		// Reset durability bars
-		for(int x = 0; x < components.length; x++) {
-			for(int y = 0; y < components[0].length; y++) {
-				og.setColor(new Color(198, 198, 198, 255));
-				og.fillRect((3 + x * 67), (65 + y * 67), 64, 3);
-			}
-		}
-		// Draw new durability bars
+		// Calculate pixel lengths for bars
 		for(ReactorComponent rc : rcs) {
 			double durability = 0;
 			int durabilityMax = 0;
@@ -128,18 +138,9 @@ public class ReactorPanel extends JPanel {
 			}
 			double granularity = durabilityMax / 64.0D;
 			int pixels = (int) Math.floor(durability / granularity);
-			if(pixels >= 32) {
-				og.setColor(Color.GREEN);
-			} else if((pixels >= 16) && (pixels < 32)) {
-				og.setColor(Color.ORANGE);
-			} else {
-				og.setColor(Color.RED);
-			}
-			final int startX = 3 + rc.getX() * 67;
-			final int startY = 65 + rc.getY() * 67;
-			og.fillRect(startX, startY, pixels, 3);
+			
+			durabilityBarLengths[rc.getX()][rc.getY()] = pixels;	
 		}
-		og.dispose();
 	}
 	
 	public void addComponent(ComponentSubType subType, int posX, int posY) {
